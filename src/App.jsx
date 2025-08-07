cb101402d7d309d8bd65d7703b9b1f0d5215b719
Karthikeyan S <karthikeya4255@gmail.com>
2025-08-07 20:05:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-server-fjcm.onrender.com");
 
 // ---------------- Encryption Helpers ----------------
 const deriveKey = async (password) => {
